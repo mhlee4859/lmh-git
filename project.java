21064285bf18668ab05b48f92cf8215c632e9994
mhlee <lmh@localhost.localdomain>
2017-10-29 22:15:31 +0900

    commit3

--- project.java
+++ project.java
@@ -2,5 +2,8 @@ public class project{
   public static void main(String args[]){
   System.out.println("homework is very hard");
   System.out.println("what the hell");
+  System.out.println("what the fuck");
+  System.out.println("what the fuck");
+  System.out.println("what the fuck");
 }
 }
